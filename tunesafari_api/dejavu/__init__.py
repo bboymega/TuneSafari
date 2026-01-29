b941e65e162b4661e8a49052795c25051351fbf1
bboymega <bboymega@git>
2026-01-29 02:13:04 +0000

    modified:   dejavu/__init__.py

--- tunesafari_api/dejavu/__init__.py
+++ tunesafari_api/dejavu/__init__.py
@@ -264,7 +264,9 @@ class Dejavu:
             # This solves the "wrong song at #1" problem by penalizing 
             # songs that have many matches but poor temporal alignment.
             total_hashes = self.db.get_song_by_id(song_id).get(FIELD_TOTAL_HASHES) or 1
-            final_score = (best_count ** 3) / (len(pairs) * np.sqrt(total_hashes))
+            length_penalty = np.log10(total_hashes)
+            alignment_density = best_count / len(pairs)
+            final_score = (best_count * alignment_density) / length_penalty
             
             songs_matches.append((song_id, best_off, best_count, best_t, final_score))
 
